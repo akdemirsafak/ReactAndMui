d511b8f6d23985291f4b2e9f9b4a8be62b31ab04
akdemirsafak <akdemirsafak@gmail.com>
2023-10-29 14:52:49 +0300

    special

--- src/components/MuiRating.tsx
+++ src/components/MuiRating.tsx
@@ -4,7 +4,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 
 export const MuiRating=()=>{
-    const [value,setValue] =useState<number| null>(null)
+    const [value,setValue] =useState<number| null>(3)
     const handleChange=(
         _event:React.ChangeEvent<{}>,
         newValue:number | null
@@ -28,6 +28,8 @@ export const MuiRating=()=>{
                 size='large'
                 icon={<FavoriteIcon fontSize="inherit" color="error"/>}
                 emptyIcon={<FavoriteBorderIcon fontSize="inherit"/>}
+                //readOnly
+                // highlightSelectedOnly
             />
         </Stack>
     )
