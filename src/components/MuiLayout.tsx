cc82d95e379212e22e55b683f12cdf8928d92d27
akdemirsafak <akdemirsafak@gmail.com>
2023-10-29 20:29:12 +0300

    Stack Completed.

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,8 +1,13 @@
-import {Box} from '@mui/material'
+import {Box,Divider,Stack} from '@mui/material'
 
 export const MuiLayout=()=>{
     return (
-        <>
+        <Stack 
+            sx={{border:'1px solid'}} 
+            direction='row' 
+            spacing={5} //Spacing ile elementler arası(Childs) boşlukları ayarlıyoruz.
+            divider={<Divider orientation='vertical' flexItem/>}
+        > 
             <Box 
                 //component='h1' 
                 sx={{
@@ -25,11 +30,11 @@ export const MuiLayout=()=>{
                 width='500px'
                 bgcolor='success.light'
                 p={2} //padding 18px
-                mt={2}
+
             //mui.com/system/properties all is working here
             >
                 Hello without Sx
             </Box>
-        </>
+        </Stack>
     )
 }
